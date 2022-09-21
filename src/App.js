b2f521231b7a0bd08a2203f1040c30a9dd01fd2d
hayatul-islam <hayatulemon340@gmail.com>
2022-09-21 19:30:21 +0600

    folder stracture

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import "./App.css";
+import TheLayout from "./pages/TheLayout";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-red-500">Hello</h1>
-    </div>
+    <>
+      <TheLayout />
+    </>
   );
 }
 
---
+++ src/pages/TheLayout.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Home";
+
+function TheLayout() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default TheLayout;
