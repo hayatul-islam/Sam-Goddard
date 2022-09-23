d3e3cce24f5e218005f1b6f347d1d97c4b6fe981
hayatul-islam <hayatulemon340@gmail.com>
2022-09-23 10:24:00 +0600

    animatin bug fixed

--- src/components/home/Work.js
+++ src/components/home/Work.js
@@ -16,20 +16,20 @@ function Work({ work, index, setIsHover, isHover }) {
 
   return (
     <>
-      <div className="relative group">
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.5, type: "spring" }}
-          whileHover={{ paddingLeft: "20px" }}
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5, type: "spring" }}
+        whileHover={{ paddingLeft: "20px" }}
+      >
+        <div
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          className={`relative group cursor-pointer z-50 ${
+            isHover && "hover:opacity-100 opacity-20"
+          }`}
         >
-          <div
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-            className={`flex justify-between  text-[#bebebe] ${
-              isHover && "hover:opacity-100 opacity-20"
-            }  `}
-          >
+          <div className={`flex justify-between  text-[#bebebe]   `}>
             <div className="flex items-start leading-none py-3 space-x-4">
               <span className="text-[46px] font-light font-neuton ">
                 ({index < 10 ? `0${index + 1}` : `${index}`})
@@ -40,20 +40,21 @@ function Work({ work, index, setIsHover, isHover }) {
               <h3 className="text-[60px] font-light font-neuton">{year}</h3>
             </div>
           </div>
-        </motion.div>
-        <hr className="opacity-20" />
-        {isShow && (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ duration: 0.5, delay: 0.2 }}
-          >
-            <div className="absolute top-[-100px] right-[5%] w-[500px] h-[350px] opacity-20">
-              <img className="w-full h-full" src={img} alt="" />
-            </div>
-          </motion.div>
-        )}
-      </div>
+
+          {isShow && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1, zIndex: 0 }}
+              transition={{ duration: 0.5, delay: 0.2 }}
+            >
+              <div className="absolute top-[-100px] right-[5%] w-[500px] h-[350px] opacity-20">
+                <img className="w-full h-full" src={img} alt="" />
+              </div>
+            </motion.div>
+          )}
+        </div>
+      </motion.div>
+      <hr className="opacity-20" />
     </>
   );
 }
