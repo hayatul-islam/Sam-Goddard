5e1b796eebecbb22154f918a614717f656d86190
hayatul-islam <hayatulemon340@gmail.com>
2022-09-24 20:51:58 +0600

    about me responsive

--- src/components/home/AboutMe.js
+++ src/components/home/AboutMe.js
@@ -3,14 +3,14 @@ import React from "react";
 function AboutMe() {
   return (
     <>
-      <div className="grid grid-cols-3 pb-20">
+      <div className="grid md:grid-cols-3 pb-20 space-y-3 md:space-y-0">
         <div>
           <h2 className="text-[18px] text-[#262525] opacity-70 font-neuton">
             ABOUT ME
           </h2>
         </div>
         <div>
-          <p className="text-[50px] text-[#262525] font-neuton leading-none ">
+          <p className="text-[33px] md:text-[40px] lg:text-[50px] text-[#262525] font-neuton leading-none ">
             &nbsp;&nbsp;&nbsp;&nbsp;Skateboard cardigan cold-pressed hella
             normcore narwhal. Occupy chartreuse art party quinoa. Hashtag forage
             pitchfork franzen waistcoat post-ironic distillery four dollar toast
