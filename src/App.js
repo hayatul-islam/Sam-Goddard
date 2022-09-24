c184f1e86222c73727269a79308e910ffa32894a
hayatul-islam <hayatulemon340@gmail.com>
2022-09-24 11:31:10 +0600

    mode control

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { createContext, useState } from "react";
 import TheLayout from "./pages/TheLayout";
+export const MainContexts = createContext();
 
 function App() {
+  const [about, setAbout] = useState(false);
+  setInterval(() => {
+    const location = window.location.pathname;
+    if (location === "/about") {
+      setAbout(true);
+    } else {
+      setAbout(false);
+    }
+  }, 1000);
+  const globalContexts = {
+    about,
+    setAbout,
+  };
   return (
     <>
-      <div className="bg-[#262525]">
-        <TheLayout />
-      </div>
+      <MainContexts.Provider value={globalContexts}>
+        <div className={`${about ? " bg-white " : "bg-[#262525]"}`}>
+          <TheLayout />
+        </div>
+      </MainContexts.Provider>
     </>
   );
 }
