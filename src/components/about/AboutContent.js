7fa18e93b06fdc3e3ac746a53e5a73eebec207d6
hayatul-islam <hayatulemon340@gmail.com>
2022-09-25 14:37:29 +0600

    about animation done

--- src/components/about/AboutContent.js
+++ src/components/about/AboutContent.js
@@ -5,7 +5,11 @@ function AboutContent() {
   return (
     <>
       <div className="py-6">
-        <motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 200 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, type: "spring" }}
+        >
           <h2 className="text-[45px] sm:text-[60px] md:text-[75px] lg:text-[98px] font-neuton leading-none font-bold max-w-6xl">
             &nbsp;&nbsp;&nbsp;&nbsp;I’m a creative developer & co-founder of
             <a
@@ -20,13 +24,20 @@ function AboutContent() {
             innovative, user-focussed, accessible & performant.
           </h2>
         </motion.div>
-        <div className="flex justify-end mt-[-100px] sm:mt[-160px] md:mt-[-250px] z-0">
-          <img
-            className="w-[90%] sm:w-[70%] md:w-[60%] lg:w-[50%] sm:h-[300px] md:h-[400px] lg:h-[500px] z-0 opacity-20 rounded"
-            src="https://www.datocms-assets.com/38619/1607792250-reform.jpg?auto=format&dpr=1&fit=crop&h=730&w=900"
-            alt=""
-          />
-        </div>
+        <motion.div
+          initial={{ scale: 1.03 }}
+          animate={{ scale: 1 }}
+          transition={{ duration: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <div className="flex justify-end mt-[-100px] sm:mt[-160px] md:mt-[-250px] z-0">
+            <img
+              className="w-[90%] sm:w-[70%] md:w-[60%] lg:w-[50%] sm:h-[300px] md:h-[400px] lg:h-[500px] z-0 opacity-20 rounded"
+              src="https://www.datocms-assets.com/38619/1607792250-reform.jpg?auto=format&dpr=1&fit=crop&h=730&w=900"
+              alt=""
+            />
+          </div>
+        </motion.div>
       </div>
     </>
   );
--- src/components/home/WorkDetails.js
+++ src/components/home/WorkDetails.js
@@ -119,9 +119,8 @@ function WorkDetails() {
               animate={{ scale: 1 }}
               transition={{ duration: 1 }}
               exit={{ opacity: 0 }}
-              className="overflow-hidden"
             >
-              <img className="w-full rounded" src={img} alt={name} />
+              <img className="w-full rounded " src={img} alt={name} />
             </motion.div>
             <motion.div
               initial={{ opacity: 0, y: 200 }}
