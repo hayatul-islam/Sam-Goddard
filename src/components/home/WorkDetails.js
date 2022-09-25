6bd3de009406681eea0373f21afbdcc749f96ff3
hayatul-islam <hayatulemon340@gmail.com>
2022-09-25 14:34:07 +0600

    work details animation done

--- src/components/home/WorkDetails.js
+++ src/components/home/WorkDetails.js
@@ -114,23 +114,35 @@ function WorkDetails() {
       <div className="px-[20px] md:px-[32px] py-12">
         <div className="grid lg:grid-cols-[auto_400px] xl:grid-cols-[auto_450px] gap-12">
           <div>
-            <div className="">
-              <img className="w-full " src={img} alt={name} />
-            </div>
-            <div className="text-[#bebebe] mt-[-110px] z-50 ">
-              <motion.div whileHover={{ opacity: 0.5 }}>
-                <Link
-                  to="/"
-                  className="text-[20px] md:text-[30px] font-neuton flex items-center"
-                >
-                  <BsArrowLeftShort className="pt-1" />
-                  <span>(All Projects)</span>
-                </Link>
-              </motion.div>
-              <h3 className="text-[60px] sm:text-[88px] lg:text-[145px] font-neue leading-none">
-                {name}
-              </h3>
-            </div>
+            <motion.div
+              initial={{ scale: 1.03 }}
+              animate={{ scale: 1 }}
+              transition={{ duration: 1 }}
+              exit={{ opacity: 0 }}
+              className="overflow-hidden"
+            >
+              <img className="w-full rounded" src={img} alt={name} />
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0, y: 200 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1, type: "spring" }}
+            >
+              <div className="text-[#bebebe] mt-[-110px] z-50 ">
+                <motion.div whileHover={{ opacity: 0.5 }}>
+                  <Link
+                    to="/"
+                    className="text-[20px] md:text-[30px] font-neuton flex items-center"
+                  >
+                    <BsArrowLeftShort className="pt-1" />
+                    <span>(All Projects)</span>
+                  </Link>
+                </motion.div>
+                <h3 className="text-[60px] sm:text-[88px] lg:text-[145px] font-neue leading-none">
+                  {name}
+                </h3>
+              </div>
+            </motion.div>
           </div>
           <div className="text-[#bebebe] space-y-12">
             <div>
@@ -141,58 +153,88 @@ function WorkDetails() {
                 <hr className="opacity-20" />
               </div>
               <div>
-                <div className="flex justify-between items-center py-1">
-                  <h4 className="text-[24px] xl:text-[34px] font-neuton ">
-                    React
-                  </h4>
-                  <h4 className="text-[18px] xl:text-[20px] font-neuton ">
-                    Static Site Generator
-                  </h4>
-                </div>
+                <motion.div
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 1, type: "spring" }}
+                >
+                  <div className="flex justify-between items-center py-1">
+                    <h4 className="text-[24px] xl:text-[34px] font-neuton ">
+                      React
+                    </h4>
+                    <h4 className="text-[18px] xl:text-[20px] font-neuton ">
+                      Static Site Generator
+                    </h4>
+                  </div>
+                </motion.div>
                 <hr className="opacity-20" />
               </div>
               <div>
-                <div className="flex justify-between items-center py-1">
-                  <h4 className="text-[24px] xl:text-[34px] font-neuton ">
-                    Tailwind
-                  </h4>
-                  <h4 className="text-[18px] xl:text-[20px] font-neuton ">
-                    CSS Framework
-                  </h4>
-                </div>
+                <motion.div
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 1, type: "spring" }}
+                >
+                  <div className="flex justify-between items-center py-1">
+                    <h4 className="text-[24px] xl:text-[34px] font-neuton ">
+                      Tailwind
+                    </h4>
+                    <h4 className="text-[18px] xl:text-[20px] font-neuton ">
+                      CSS Framework
+                    </h4>
+                  </div>
+                </motion.div>
                 <hr className="opacity-20" />
               </div>
               <div>
-                <div className="flex justify-between items-center py-1">
-                  <h4 className="text-[24px] xl:text-[34px] font-neuton ">
-                    Framer Motion
-                  </h4>
-                  <h4 className="text-[18px] xl:text-[20px] font-neuton ">
-                    Animation
-                  </h4>
-                </div>
+                <motion.div
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 1, type: "spring" }}
+                >
+                  <div className="flex justify-between items-center py-1">
+                    <h4 className="text-[24px] xl:text-[34px] font-neuton ">
+                      Framer Motion
+                    </h4>
+                    <h4 className="text-[18px] xl:text-[20px] font-neuton ">
+                      Animation
+                    </h4>
+                  </div>
+                </motion.div>
                 <hr className="opacity-20" />
               </div>
               <div>
-                <div className="flex justify-between items-center py-1">
-                  <h4 className="text-[24px] xl:text-[34px] font-neuton ">
-                    Sanity
-                  </h4>
-                  <h4 className="text-[18px] xl:text-[20px] font-neuton ">
-                    Headless CMS
-                  </h4>
-                </div>
+                <motion.div
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 1, type: "spring" }}
+                >
+                  <div className="flex justify-between items-center py-1">
+                    <h4 className="text-[24px] xl:text-[34px] font-neuton ">
+                      Sanity
+                    </h4>
+                    <h4 className="text-[18px] xl:text-[20px] font-neuton ">
+                      Headless CMS
+                    </h4>
+                  </div>
+                </motion.div>
                 <hr className="opacity-20" />
               </div>
               <div>
-                <div className="flex justify-between items-center py-1">
-                  <h4 className="text-[24px] xl:text-[34px] font-neuton ">
-                    Vercel
-                  </h4>
-                  <h4 className="text-[18px] xl:text-[20px] font-neuton ">
-                    Hosting
-                  </h4>
-                </div>
+                <motion.div
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 1, type: "spring" }}
+                >
+                  <div className="flex justify-between items-center py-1">
+                    <h4 className="text-[24px] xl:text-[34px] font-neuton ">
+                      Vercel
+                    </h4>
+                    <h4 className="text-[18px] xl:text-[20px] font-neuton ">
+                      Hosting
+                    </h4>
+                  </div>
+                </motion.div>
                 <hr className="opacity-20" />
               </div>
             </div>
@@ -205,17 +247,23 @@ function WorkDetails() {
                 <hr className="opacity-20" />
               </div>
               <div>
-                <div className="flex justify-between items-center py-1">
-                  <h4 className="text-[24px] xl:text-[34px] font-neuton ">
-                    Designer
-                  </h4>
-                  <Link
-                    to=""
-                    className="text-[18px] xl:text-[20px] font-neuton hover:line-through decoratin-1 "
-                  >
-                    Isaac Powell
-                  </Link>
-                </div>
+                <motion.div
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 1, type: "spring" }}
+                >
+                  <div className="flex justify-between items-center py-1">
+                    <h4 className="text-[24px] xl:text-[34px] font-neuton ">
+                      Designer
+                    </h4>
+                    <Link
+                      to=""
+                      className="text-[18px] xl:text-[20px] font-neuton hover:line-through decoratin-1 "
+                    >
+                      Isaac Powell
+                    </Link>
+                  </div>
+                </motion.div>
                 <hr className="opacity-20" />
               </div>
             </div>
