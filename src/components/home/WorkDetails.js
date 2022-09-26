92af0f9322677e23bbde85076cda6e59d5088cd3
hayatul-islam <hayatulemon340@gmail.com>
2022-09-26 12:19:08 +0600

    data updated

--- src/components/home/WorkDetails.js
+++ src/components/home/WorkDetails.js
@@ -49,7 +49,7 @@ const works = [
     id: 5,
     name: "Vegan twee",
     img: "https://images.unsplash.com/photo-1519219788971-8d9797e0928e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTk0fHxzaW1wbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-    year: 2021,
+    year: 2020,
     details:
       "Vegan twee squid live-edge irony meditation. Actually intelligentsia cronut seitan gatekeep heirloom tacos vaporware cloud bread +1 bicycle rights YOLO. Kitsch DIY ennui hot chicken, drinking vinegar praxis keytar coloring book pop-up authentic.",
   },
@@ -57,7 +57,7 @@ const works = [
     id: 6,
     name: "Jianbing",
     img: "https://images.unsplash.com/photo-1577017050748-389f0021d860?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTg2fHxzaW1wbGV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-    year: 2021,
+    year: 2020,
     details:
       "Jianbing iceland praxis bespoke food truck blue bottle la croix synth art party edison bulb wolf keytar. Gochujang coloring book kinfolk poutine pitchfork farm-to-table.",
   },
@@ -68,20 +68,40 @@ const works = [
     year: 2021,
     details:
       "Trust fund marfa pour-over, mukbang locavore tote bag iceland artisan godard. Knausgaard typewriter woke put a bird on it williamsburg, squid literally truffaut freegan trust fund tilde plaid. Kickstarter quinoa vegan big mood unicorn. Echo park fixie sartorial helvetica photo booth YOLO pitchfork,",
+    stack: {
+      twig: "Templating",
+      craft: "CMS",
+      tailwind: "CMS Framework",
+      algolia: "Live Search",
+    },
+    thanksTo: {
+      client: "V&A",
+      employer: "Kind",
+    },
   },
   {
     id: 8,
     name: "Chartreuse",
     img: "https://images.unsplash.com/photo-1509943089014-50b4f4edfbbb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzV8fHNpbXBsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-    year: 2021,
+    year: 2019,
     details:
       "single-origin coffee edison bulb glossier chartreuse iPhone +1. Taiyaki photo booth 90's lyft cold-pressed chia. Flexitarian sustainable wolf schlitz farm-to-table. Banh mi artisan tousled, 90's helvetica roof party fashion axe.",
+    stack: {
+      twig: "Templating",
+      craft: "CMS",
+      tailwind: "CMS Framework",
+      algolia: "Live Search",
+    },
+    thanksTo: {
+      client: "V&A",
+      employer: "Kind",
+    },
   },
   {
     id: 9,
     name: "Pabst XOXO",
     img: "https://images.unsplash.com/photo-1584117756263-bc482c509c49?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzJ8fHNpbXBsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-    year: 2021,
+    year: 2019,
     details:
       "Pabst XOXO ramps, art party fashion axe readymade voluptate hexagon salvia 3 wolf moon church-key ut bushwick synth JOMO. Fanny pack taiyaki normcore XOXO, quinoa bruh lo-fi heirloom nulla blog cornhole. ",
   },
@@ -89,7 +109,7 @@ const works = [
     id: 10,
     name: "Lyft pinterest",
     img: "https://images.unsplash.com/photo-1521489871110-81dc5a61dbda?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzd8fHNpbXBsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-    year: 2021,
+    year: 2020,
     details:
       "Lyft pinterest vexillologist selvage, shaman elit vaporware mixtape kinfolk forage. Kitsch palo santo 90's, poke chambray edison bulb kombucha succulents laborum pinterest blue bottle sustainable. Whatever fanny pack pok pok fingerstache. Freegan intelligentsia blue bottle single-origin coffee anim lyft, kombucha truffaut.",
   },
@@ -97,15 +117,25 @@ const works = [
     id: 11,
     name: "Brunch",
     img: "https://images.unsplash.com/photo-1448932284983-0c7b152eba33?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fHNpbXBsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-    year: 2021,
+    year: 2019,
     details:
       "Brunch vaporware selvage proident truffaut bushwick intelligentsia taiyaki cliche flexitarian ascot incididunt air plant vegan af. Elit proident 90's fixie enim try-hard distillery ugh prism heirloom ut. Blog aliquip migas cloud bread chillwave waistcoat viral hammock williamsburg dolore af chambray anim.",
+    stack: {
+      twig: "Templating",
+      craft: "CMS",
+      tailwind: "CMS Framework",
+      algolia: "Live Search",
+    },
+    thanksTo: {
+      client: "V&A",
+      employer: "Kind",
+    },
   },
   {
     id: 12,
     name: "Aliqua",
     img: "https://images.unsplash.com/photo-1513506003901-1e6a229e2d15?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fHNpbXBsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-    year: 2021,
+    year: 2018,
     details:
       "Aliqua incididunt austin aliquip bicycle rights anim enim. Lorem fixie 90's drinking vinegar, pour-over seitan la croix ad magna mustache tumblr. Scenester readymade health goth seitan twee venmo humblebrag fashion axe exercitation.",
   },
